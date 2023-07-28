8ee55aa50dec6dd255647321b8c1bb0a2c97f96e
FelixdenBreejen <FelixdenBreejen@hotmail.com>
2023-07-28 12:24:16 +0900

    launch agent tmux bash script has now flags

--- src/launch_benchmarks/launch_agent_tmux.sh
+++ src/launch_benchmarks/launch_agent_tmux.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-tmux new-session -d "conda activate tabularbench; cd src; export CUDA_VISIBLE_DEVICES=$1; wandb agent felixatkaist/mlp_pwl_benchmark/lltsl4zk"
+while getopts g:s: flag
+do
+    case "${flag}" in
+        g) gpu=${OPTARG};;
+        s) sweep=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+tmux new-session -d "conda activate tabularbench; cd src; export CUDA_VISIBLE_DEVICES=$gpu; wandb agent felixatkaist/mlp_pwl_benchmark/$sweep"
