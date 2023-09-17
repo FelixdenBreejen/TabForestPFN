b7e92211e32cee5d55ae85cce3c565cb54984296
Felix den Breejen <FelixdenBreejen@hotmail.com>
2023-09-17 15:45:22 +0900

    Nothing changed

--- tabularbench/sweeps/paths_and_filenames.py
+++ tabularbench/sweeps/paths_and_filenames.py
@@ -2,4 +2,5 @@ SWEEP_FILE_NAME = 'sweep.csv'                                      # Every sweep
 RESULTS_FILE_NAME = 'results.csv'                                  # Results for one sweep
 RESULTS_MODIFIED_FILE_NAME = 'results_modified_for_plotting.csv'   # Same as above, but slighly modified
 DEFAULT_RESULTS_FILE_NAME = 'default_results.csv'                  # Results for default configuration runs 
-PATH_TO_ALL_BENCH_CSV = 'analyses/results/benchmark_total.csv'     # Benchmark results from benchmark authors
+PATH_TO_ALL_BENCH_CSV = 'data/benchmark_total.csv'     # Benchmark results from benchmark authors
+PATH_TO_DATA_SPLIT = 'tabularbench/data/train_val_test_indices.npy' # Indices for train, val, test split
