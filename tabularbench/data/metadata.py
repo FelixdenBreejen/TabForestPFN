bf07df90db9b8028acebbb0946aa79ba3fc3b8fb
Felix den Breejen <FelixdenBreejen@hotmail.com>
2024-04-16 16:25:56 +0900

    Add classes to metadata

--- tabularbench/data/metadata.py
+++ tabularbench/data/metadata.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import xarray as xr
 
@@ -31,15 +32,17 @@ def create_metadata_(list_of_dataset_paths: list[Path]) -> pd.DataFrame:
             'openml_dataset_id': ds.attrs['openml_dataset_id'],
             'openml_dataset_name': ds.attrs['openml_dataset_name'],
             'n_observations': ds.sizes['observation'],
-            'n_features': ds.sizes['feature'],
-            'n_splits': ds.sizes['split'],
             'n_train': ds['split_index_train'].sel(split=0).sum().item(),
             'n_val': ds['split_index_val'].sel(split=0).sum().item(),
             'n_test': ds['split_index_test'].sel(split=0).sum().item(),
+            'n_features': ds.sizes['feature'],
+            'n_splits': ds.sizes['split'],
+            'n_classes': len(np.unique(ds['y']))
+
         })
 
-    metadata = pd.DataFrame(metadata)
-    metadata.set_index('openml_dataset_id', inplace=True)
-    metadata.sort_index(inplace=True)
+    df = pd.DataFrame(metadata)
+    df.set_index('openml_dataset_id', inplace=True)
+    df.sort_index(inplace=True)
 
-    return metadata
+    return df
